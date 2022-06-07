33755dcf9c6d4b63d2f861756e1a09e9413c31ee
Liam-rng <liam.scott.7798@gmail.com>
2022-06-07 17:05:18 -0400

    Use slice method to reverse a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    var reversed_str = '';
+    for (let index = (str.length - 1); index >= 0; index--) {
+        distance = index + 1;
+        reversed_str += str.slice(index, distance);
+    }
+    return reversed_str;
 };
 
 // Do not edit below this line
