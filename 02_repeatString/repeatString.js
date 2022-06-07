9c20f0fe2a5a625f9a89139adf5c406ff117f4ec
Liam-rng <liam.scott.7798@gmail.com>
2022-06-07 16:13:31 -0400

    Use for loop to solve exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
 
+const repeatString = function (str, n_of_times) {
+    newStr = "";
+    if (n_of_times < 0)
+        return "ERROR";
+    for (let i = 0; i < n_of_times; i++) {
+        newStr += str;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
