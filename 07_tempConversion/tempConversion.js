6a8e87d5674eb817207b4cd82d3c7433a7ab6a7f
Liam-rng <liam.scott.7798@gmail.com>
2022-06-08 02:29:09 -0400

    Use if else statement for leapYears and math.round for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function (arg1) {
+  return Math.round(10 * (arg1 - 32) * 5 / 9) / 10.0
 };
 
-const ctof = function() {
-
+const ctof = function (arg1) {
+  return Math.round(10 * (arg1 * 9 / 5 + 32)) / 10.0
 };
 
 // Do not edit below this line
