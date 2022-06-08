bfeb9089f96d77f2a792f26dc20fa330b9310aef
Liam-rng <liam.scott.7798@gmail.com>
2022-06-08 00:07:51 -0400

    Use if else statement and destructuring assignment to swap 2 variables

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (arg1, arg2) {
+    let sum = 0;
+    if (typeof arg1 !== typeof arg2 || typeof arg1 !== "number")
+        return "ERROR";
+    else if (arg1 < 0 || arg2 < 0)
+        return "ERROR";
+    else if (arg1 > arg2)
+        [arg1, arg2] = [arg2, arg1];
+    for (let i = arg1; i <= arg2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
